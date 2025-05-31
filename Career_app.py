3711e981cb11de4757654572d81eefadd82765c4
Darfisha Shaikh <darfisha2002@gmail.com>
2025-06-01 03:09:00 +0530

    Update Career_app.py

--- Career_app.py
+++ Career_app.py
@@ -6,7 +6,7 @@ import random
 
 # Load the dataset
 df = pd.read_excel("career_data.xlsx")
-df['Required_skills'] = df['Required_skills'].apply(lambda x: [skill.strip().lower() for skill in x.split(',')])
+df['Required skills'] = df['Required skills'].apply(lambda x: [skill.strip().lower() for skill in x.split(',')])
 
 # Fun background colors
 page_bg_img = '''
